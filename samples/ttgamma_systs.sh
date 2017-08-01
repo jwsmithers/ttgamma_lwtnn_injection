39c122ed462372c3e30f36d90117b9cc6703a64f
jwsmith <joshua.wyatt.smith@cern.ch>
2017-08-01 18:09:59 +0200

    ttgamma syst ntuples to run over script

---
+++ samples/ttgamma_systs.sh
@@ -0,0 +1,5 @@
+./nnInjector 410082.ttgamma_noallhad.p2952.v008.001.root
+./nnInjector 410082.ttgamma_noallhad.p2952.v008.002.root
+./nnInjector 410082.ttgamma_noallhad.p2952.v008.003.root
+./nnInjector 410082.ttgamma_noallhad.p2952.v008.004.root
+./nnInjector 410082.ttgamma_noallhad.p2952.v008.005.root
