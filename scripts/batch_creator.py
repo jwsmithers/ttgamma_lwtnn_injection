07930a1382ba3c981cceda99f9badc85526af435
jwsmith <joshua.wyatt.smith@cern.ch>
2017-08-12 19:00:11 +0200

    scrip to create batch scripts

---
+++ scripts/batch_creator.py
@@ -0,0 +1,23 @@
+path="/eos/user/c/caudron/TtGamma_ntuples/v009/CR1S/ejets"
+channel="ejets"
+nn_file="model4_100_singlelepton_ELD.json"
+
+
+print """
+executable            = /afs/cern.ch/work/j/jwsmith/github/ttgamma_lwtnn_injection/build_batch/"""+channel+"""/nnInjector
+transfer_input_files  = """ + nn_file +  """
+getenv                = True
++JobFlavour           = "testmatch"
+"""
+
+from os import listdir
+from os.path import isfile, join
+ntuples = [f for f in listdir(path) if isfile(join(path, f))]
+
+for i in ntuples:
+  print "output = output/$(ClusterId).$(ProcId)."+i.rstrip(".root")+".out"
+  print "error = error/$(ClusterId).$(ProcId)."+i.rstrip(".root")+".err"
+  print "log = log/$(ClusterId).$(ProcId)."+i.rstrip(".root")+".log"
+  print "arguments = "+i
+  print "queue"
+  print "\n"
