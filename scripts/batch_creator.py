4b7d209a5f04b863fe117b3a60e4506dc194fc70
jwsmith <joshua.wyatt.smith@cern.ch>
2017-08-12 19:28:56 +0200

    minor tweaks

--- scripts/batch_creator.py
+++ scripts/batch_creator.py
@@ -7,6 +7,7 @@ print """
 executable            = /afs/cern.ch/work/j/jwsmith/github/ttgamma_lwtnn_injection/build_batch/"""+channel+"""/nnInjector
 transfer_input_files  = """ + nn_file +  """
 getenv                = True
+JobBatchName	      = """ + channel + """
 +JobFlavour           = "testmatch"
 """
 
@@ -15,9 +16,10 @@ from os.path import isfile, join
 ntuples = [f for f in listdir(path) if isfile(join(path, f))]
 
 for i in ntuples:
-  print "output = output/$(ClusterId).$(ProcId)."+i.rstrip(".root")+".out"
-  print "error = error/$(ClusterId).$(ProcId)."+i.rstrip(".root")+".err"
-  print "log = log/$(ClusterId).$(ProcId)."+i.rstrip(".root")+".log"
-  print "arguments = "+i
-  print "queue"
-  print "\n"
+  if "ttgamma" in i:
+    print "output = output/$(ClusterId).$(ProcId)."+i.rstrip(".root")+".out"
+    print "error = error/$(ClusterId).$(ProcId)."+i.rstrip(".root")+".err"
+    print "log = log/$(ClusterId).$(ProcId)."+i.rstrip(".root")+".log"
+    print "arguments = "+i
+    print "queue"
+    print "\n"
