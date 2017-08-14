41c39cd6344bd427c935361308273fbd63e6b34d
jwsmith <joshua.wyatt.smith@cern.ch>
2017-08-14 10:59:12 +0200

    Run over nominal tree first, then get the rest if there are any

--- Root/nnInjector.cxx
+++ Root/nnInjector.cxx
@@ -190,8 +190,8 @@ int main(int argc, char** argv)
 {
   gROOT->ProcessLine( "gErrorIgnoreLevel = kFatal;");
   std::cout << "Found " << argc-1 << " files to run over:" << std::endl;
-  //std::string in_file_name=("../json/model4_300_dilepton_ELD.json");
-  std::string in_file_name=("../json/model4_100_singlelepton_ELD.json");
+  std::string in_file_name=("model4_300_dilepton_ELD.json");
+  //std::string in_file_name=("model4_100_singlelepton_ELD.json");
   std::ifstream in_file(in_file_name);
 
   if(!in_file){
@@ -200,18 +200,23 @@ int main(int argc, char** argv)
 
   // path to ntuples from AnalysisTop
   // Where we read from:
-  string path = "root://eosuser//eos/user/c/caudron/TtGamma_ntuples/v009/CR1/";
+  string path = "root://eosuser//eos/user/c/caudron/TtGamma_ntuples/v009/CR1S/";
+  //string path = "/eos/user/c/caudron/TtGamma_ntuples/v009/CR1S/";
   //string path = "root://eosatlas//eos/atlas/user/j/jwsmith/reprocessedNtuples/v009/QE2_yichen/";
-  //string channels[] ={"ee","emu","mumu"};
-   string channels[] ={"ejets"};
+  string channels[] ={"ee","emu","mumu"};
+  //string channels[] ={"mujets"};
 
   // Where we save to:
-  string myPath = "../CR1/";
+  string myPath = "root://eosatlas//eos/atlas/user/j/jwsmith/reprocessedNtuples/v009_flattened/CR1S/";
+  //string myPath = "/eos/atlas/user/j/jwsmith/reprocessedNtuples/v009_flattened/CR1S/";
+  //string myPath = "../CR1/";
 
   TFile *newfile;
   TFile *oldFile;
   TTree *newtree;
+  TTree *newtree_nominal;
   TChain *fChain;
+  TChain *fChain_nominal;
 
 
   m_config_netFile = new lwt::JSONConfig(lwt::parse_json(in_file));
@@ -228,8 +233,35 @@ int main(int argc, char** argv)
       std::cout<<c<<": "<< filename<< std::endl;
       std::cout<<c<<": Saving to "<<newpath<< std::endl;
 
+      newfile = new TFile((newpath.c_str()), "update");
+      // Nominal tree
+      if (filename.find("QCDfakes") != std::string::npos) {
+        fChain_nominal = new TChain("nominal_Loose");
+      }
+      else {
+        fChain_nominal = new TChain("nominal");
+      }
+ 
+      fChain_nominal->Add((file).c_str());
+
+
+      newtree_nominal = fChain_nominal->CloneTree(0);
+      if(fChain_nominal->GetEntries() == 0){
+        std::cout<<"No events, skipping"<<std::endl;
+        continue;
+      }
+      printf("#################################################\n");
+      printf("Currently working on nominal tree (outside loop) \n");
+      printf("#################################################\n");
+      m_add_branches(fChain_nominal,newtree_nominal,m_neuralNet);
+
+      newfile->cd();
+      newtree_nominal->Write();
+      //
+
       oldFile = new TFile((file.c_str()), "read");
 
+      // Do a loop over all trees that are not nominal
       TList* list = oldFile->GetListOfKeys() ;
       if (!list) { printf("<E> No keys found in file\n") ; exit(1) ; }
       TIter next(list) ;
@@ -239,13 +271,13 @@ int main(int argc, char** argv)
       while ( key = (TKey*)next() ) {
 
         obj = key->ReadObj() ;
-        if ( (strcmp(obj->IsA()->GetName(),"TTree")!=0) || (strcmp("sumWeights",obj->GetName()) == 0) ) {
+        if ( (strcmp(obj->IsA()->GetName(),"TTree")!=0) || (strcmp("sumWeights",obj->GetName()) == 0) 
+          || (strcmp("nominal",obj->GetName()) == 0) ) {
           printf("Not running over: %s \n",obj->GetName()); continue; 
         }
         printf("#####################################\n");
         printf("Currently working on %s \n",obj->GetName());
         printf("#####################################\n");
-
 
         newfile = new TFile((newpath.c_str()), "update");
         fChain = new TChain(obj->GetName());
