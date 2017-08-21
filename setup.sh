85e6d6255798349dc06b17c6f75a3f4d00a34728
jwsmith <joshua.wyatt.smith@cern.ch>
2017-08-21 14:50:48 +0200

    tweak setup script

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ lsetup "cmake 3.6.0"
 
 lsetup "lcgenv -p LCG_88 x86_64-slc6-gcc49-opt ROOT"
 #lsetup "root 6.10.02-x86_64-slc6-gcc62-opt"
-lsetup "lcgenv -p LCG_88 x86_64-slc6-gcc49-opt gdb"
+#lsetup "lcgenv -p LCG_88 x86_64-slc6-gcc49-opt gdb"
 
 localSetupSFT -q releases/LCG_88/eigen/3.2.9 --cmtconfig x86_64-slc6-gcc49-opt
 localSetupBoost -q boost-1.60.0-python2.7-x86_64-slc6-gcc49
