60270068c7e0c02edb9970ee4f6c1b243e57c488
Sara Ghasemi <sara.ghasemi@cern.ch>
2018-02-22 21:29:44 +0100

    Added h-fake sf to nnInjector

--- Root/nnInjector.cxx
+++ Root/nnInjector.cxx
@@ -110,6 +110,46 @@ void m_add_efake_sf(
   efake_sf_applied = true;
 }
 
+void m_add_hadFake_sf( TFile *file, std::string source )
+{
+  // Open the hadronic fake file
+  TFile* _f_hfake_SFs = new TFile(source.c_str(),"read");
+  if ( _f_hfake_SFs->IsOpen() ) std::cout << source << " file opened successfully"
+      << std::endl;
+  
+  // Get the SF historams
+  TH1F* hfakeSF_conv_Nominal = (TH1F*)_f_hfake_SFs->Get("hfakeSF_conv_Nominal");
+  TH1F* hfakeSF_conv_ttbarModelling_Sherpa = (TH1F*)_f_hfake_SFs->Get("hfakeSF_conv_ttbarModelling_Sherpa");
+  TH1F* hfakeSF_conv_ttbarModelling_ISRFSRUp = (TH1F*)_f_hfake_SFs->Get("hfakeSF_conv_ttbarModelling_ISRFSRUp");
+  TH1F* hfakeSF_conv_ttbarModelling_ISRFSRDown = (TH1F*)_f_hfake_SFs->Get("hfakeSF_conv_ttbarModelling_ISRFSRDown");
+  TH1F* hfakeSF_conv_BkgSubtraction_ttgamma = (TH1F*)_f_hfake_SFs->Get("hfakeSF_conv_BkgSubtraction_ttgamma");
+  TH1F* hfakeSF_conv_BkgSubtraction_rest = (TH1F*)_f_hfake_SFs->Get("hfakeSF_conv_BkgSubtraction_rest");
+  TH1F* hfakeSF_unconv_Nominal = (TH1F*)_f_hfake_SFs->Get("hfakeSF_unconv_Nominal");
+  TH1F* hfakeSF_unconv_ttbarModelling_Sherpa = (TH1F*)_f_hfake_SFs->Get("hfakeSF_unconv_ttbarModelling_Sherpa");
+  TH1F* hfakeSF_unconv_ttbarModelling_ISRFSRUp = (TH1F*)_f_hfake_SFs->Get("hfakeSF_unconv_ttbarModelling_ISRFSRUp");
+  TH1F* hfakeSF_unconv_ttbarModelling_ISRFSRDown = (TH1F*)_f_hfake_SFs->Get("hfakeSF_unconv_ttbarModelling_ISRFSRDown");
+  TH1F* hfakeSF_unconv_BkgSubtraction_ttgamma = (TH1F*)_f_hfake_SFs->Get("hfakeSF_unconv_BkgSubtraction_ttgamma");
+  TH1F* hfakeSF_unconv_BkgSubtraction_rest = (TH1F*)_f_hfake_SFs->Get("hfakeSF_unconv_BkgSubtraction_rest");
+  
+  // Save them
+  file->cd();
+  hfakeSF_conv_Nominal->Draw(); hfakeSF_conv_Nominal->Write();
+  hfakeSF_conv_ttbarModelling_Sherpa->Draw(); hfakeSF_conv_ttbarModelling_Sherpa->Write();
+  hfakeSF_conv_ttbarModelling_ISRFSRUp->Draw(); hfakeSF_conv_ttbarModelling_ISRFSRUp->Write();
+  hfakeSF_conv_ttbarModelling_ISRFSRDown->Draw(); hfakeSF_conv_ttbarModelling_ISRFSRDown->Write();
+  hfakeSF_conv_BkgSubtraction_ttgamma->Draw(); hfakeSF_conv_BkgSubtraction_ttgamma->Write();
+  hfakeSF_conv_BkgSubtraction_rest->Draw(); hfakeSF_conv_BkgSubtraction_rest->Write();
+  hfakeSF_unconv_ttbarModelling_Sherpa->Draw(); hfakeSF_unconv_ttbarModelling_Sherpa->Write();
+  hfakeSF_unconv_ttbarModelling_ISRFSRUp->Draw(); hfakeSF_unconv_ttbarModelling_ISRFSRUp->Write();
+  hfakeSF_unconv_ttbarModelling_ISRFSRDown->Draw(); hfakeSF_unconv_ttbarModelling_ISRFSRDown->Write();
+  hfakeSF_unconv_BkgSubtraction_ttgamma->Draw(); hfakeSF_unconv_BkgSubtraction_ttgamma->Write();
+  hfakeSF_unconv_BkgSubtraction_rest->Draw(); hfakeSF_unconv_BkgSubtraction_rest->Write();
+  std::cout<<"Added hadronic fake SFs..."<<std::endl;
+  
+  // To be used later in eventloop
+  hadFake_sf_applied = true;
+}
+
 void m_add_branches(
   TChain *fChain_func, 
   TTree *newT,
@@ -130,6 +170,11 @@ void m_add_branches(
   }
 
   newT->Branch("ph_kfactor_correct","vector<float>",&m_ph_kfactor_correct);
+  if(kfactor_applied){
+    _h_kfactor_sl = (TH1F*)file->Get("kfactor_nlo_mt_ph_pt_fine_SL");
+    _h_kfactor_dl = (TH1F*)file->Get("kfactor_nlo_mt_ph_pt_fine_DL");
+  }
+  
   newT->Branch("efake_sf_Nominal","vector<float>",&m_efake_sf_Nominal);
   newT->Branch("efake_sf_RangeDown","vector<float>",&m_efake_sf_RangeDown);
   newT->Branch("efake_sf_BkgFunc","vector<float>",&m_efake_sf_BkgFunc);
@@ -142,10 +187,6 @@ void m_add_branches(
   newT->Branch("efake_sf_Stat_Pt4Eta3","vector<float>",&m_efake_sf_Stat_Pt4Eta3);
   newT->Branch("efake_sf_Stat_Pt4Eta5","vector<float>",&m_efake_sf_Stat_Pt4Eta5);
   newT->Branch("efake_sf_Total","vector<float>",&m_efake_sf_Total);
-  if(kfactor_applied){
-    _h_kfactor_sl = (TH1F*)file->Get("kfactor_nlo_mt_ph_pt_fine_SL");
-    _h_kfactor_dl = (TH1F*)file->Get("kfactor_nlo_mt_ph_pt_fine_DL");
-  }
   if(efake_sf_applied){
     _h_efake_sf_Nominal = (TH1F*)file->Get("Final_EFake_SF_Nominal");
     _h_efake_sf_RangeDown = (TH1F*)file->Get("Final_EFake_SF_RangeDown");
@@ -159,6 +200,27 @@ void m_add_branches(
     _h_efake_sf_Stat_Pt4Eta3 = (TH1F*)file->Get("Final_EFake_SF_Stat_Pt4Eta3");
     _h_efake_sf_Stat_Pt4Eta5 = (TH1F*)file->Get("Final_EFake_SF_Stat_Pt4Eta5");
     _h_efake_sf_Total = (TH1F*)file->Get("Final_EFake_SF_Total");
+  }
+    
+  newT->Branch("hadFake_sf_Nominal","vector<float>",&m_hadFake_sf_Nominal);
+  newT->Branch("hadFake_sf_ttbarModelling_Sherpa","vector<float>",&m_hadFake_sf_ttbarModelling_Sherpa);
+  newT->Branch("hadFake_sf_ttbarModelling_ISRFSRUp","vector<float>",&m_hadFake_sf_ttbarModelling_ISRFSRUp);
+  newT->Branch("hadFake_sf_ttbarModelling_ISRFSRDn","vector<float>",&m_hadFake_sf_ttbarModelling_ISRFSRDn);
+  newT->Branch("hadFake_sf_BkgSubtraction_ttgamma","vector<float>",&m_hadFake_sf_BkgSubtraction_ttgamma);
+  newT->Branch("hadFake_sf_BkgSubtraction_rest","vector<float>",&m_hadFake_sf_BkgSubtraction_rest);
+  if(hadFake_sf_applied){
+    _hadFake_sf_conv_Nominal = (TH1F*)file->Get("hfakeSF_conv_Nominal");
+    _hadFake_sf_conv_ttbarModelling_Sherpa = (TH1F*)file->Get("hfakeSF_conv_ttbarModelling_Sherpa");
+    _hadFake_sf_conv_ttbarModelling_ISRFSRUp = (TH1F*)file->Get("hfakeSF_conv_ttbarModelling_ISRFSRUp");
+    _hadFake_sf_conv_ttbarModelling_ISRFSRDown = (TH1F*)file->Get("hfakeSF_conv_ttbarModelling_ISRFSRDown");
+    _hadFake_sf_conv_BkgSubtraction_ttgamma = (TH1F*)file->Get("hfakeSF_conv_BkgSubtraction_ttgamma");
+    _hadFake_sf_conv_BkgSubtraction_rest = (TH1F*)file->Get("hfakeSF_conv_BkgSubtraction_rest");
+    _hadFake_sf_unconv_Nominal = (TH1F*)file->Get("hfakeSF_unconv_Nominal");
+    _hadFake_sf_unconv_ttbarModelling_Sherpa = (TH1F*)file->Get("hfakeSF_unconv_ttbarModelling_Sherpa");
+    _hadFake_sf_unconv_ttbarModelling_ISRFSRUp = (TH1F*)file->Get("hfakeSF_unconv_ttbarModelling_ISRFSRUp");
+    _hadFake_sf_unconv_ttbarModelling_ISRFSRDown = (TH1F*)file->Get("hfakeSF_unconv_ttbarModelling_ISRFSRDown");
+    _hadFake_sf_unconv_BkgSubtraction_ttgamma = (TH1F*)file->Get("hfakeSF_unconv_BkgSubtraction_ttgamma");
+    _hadFake_sf_unconv_BkgSubtraction_rest = (TH1F*)file->Get("hfakeSF_unconv_BkgSubtraction_rest");
   }
 
   newT->Branch("jet_pt_1st_correct",&m_jet_pt_1st_correct);   
@@ -182,7 +244,7 @@ void m_add_branches(
   activateBranches(fChain_func);
 
   for (int event = 0; event < nentries; event++) {
-
+    
     fChain_func->GetEntry(event);
     loadBar(event, nentries, 100, 50);
 
@@ -289,6 +351,12 @@ void m_add_branches(
     m_efake_sf_Stat_Pt4Eta3->resize(ph_pt->size());
     m_efake_sf_Stat_Pt4Eta5->resize(ph_pt->size());
     m_efake_sf_Total->resize(ph_pt->size());
+    m_hadFake_sf_Nominal->resize(ph_pt->size());
+    m_hadFake_sf_ttbarModelling_Sherpa->resize(ph_pt->size());
+    m_hadFake_sf_ttbarModelling_ISRFSRUp->resize(ph_pt->size());
+    m_hadFake_sf_ttbarModelling_ISRFSRDn->resize(ph_pt->size());
+    m_hadFake_sf_BkgSubtraction_ttgamma->resize(ph_pt->size());
+    m_hadFake_sf_BkgSubtraction_rest->resize(ph_pt->size());
 
     for(int photon = 0 ; photon < ph_pt->size(); photon++){
       m_event_ELD_MVA_all_correct->at(photon)=-99;
@@ -333,82 +401,138 @@ void m_add_branches(
         //std::cout<<"MVA all = "<< out.second << std::endl;
         m_event_ELD_MVA_all_correct->at(photon) = out.second;
       }
+      
+      // How we handle PPT weights
+      if(ppt_systematics_applied){
+        float PPT_x_value = ph_HFT_MVA->at(photon);
 
+        // Hfake derive fit weight
+        TF1 *_ppt_hfake_fit = (TF1*)_ppt_hfake->GetFunction("pol1");
+        m_weight_PPT_hfake_fit = _ppt_hfake_fit->Eval(PPT_x_value);
+        // Hfake derive bin weight
+        int hfake_bin_number = _ppt_hfake->GetXaxis()->FindBin(PPT_x_value);
+        m_weight_PPT_hfake_bin = _ppt_hfake->GetBinContent(hfake_bin_number);
 
-    // How we handle PPT weights
-    if(ppt_systematics_applied){
-    float PPT_x_value = ph_HFT_MVA->at(photon);
-
-    // Hfake derive fit weight
-    TF1 *_ppt_hfake_fit = (TF1*)_ppt_hfake->GetFunction("pol1");
-    m_weight_PPT_hfake_fit = _ppt_hfake_fit->Eval(PPT_x_value);
-    // Hfake derive bin weight
-    int hfake_bin_number = _ppt_hfake->GetXaxis()->FindBin(PPT_x_value);
-    m_weight_PPT_hfake_bin = _ppt_hfake->GetBinContent(hfake_bin_number);
-
-    // Prompt derive fit weight
-    TF1 *_ppt_prompt_fit = (TF1*)_ppt_prompt->GetFunction("pol1");
-    m_weight_PPT_prompt_fit = _ppt_prompt_fit->Eval(PPT_x_value);
-    // Prompt derive bin weight
-    int promt_bin_number = _ppt_prompt->GetXaxis()->FindBin(PPT_x_value);
-    m_weight_PPT_prompt_bin = _ppt_prompt->GetBinContent(promt_bin_number);
-
-    }
-    delete _ppt_hfake_fit;
-    delete _ppt_prompt_fit;
-
-    // Add in the Kfactor weights
-    m_ph_kfactor_correct->at(photon)=1;
-    if(kfactor_applied){
-      float photon_pt = ph_pt->at(photon)*0.001;
-      if(is_singlelepton){
-        int kfactor_pt_bin_number = _h_kfactor_sl->GetXaxis()->FindBin(photon_pt);
-        if (photon_pt >= _h_kfactor_sl->GetXaxis()->GetXmax())
-          kfactor_pt_bin_number = _h_kfactor_sl->GetXaxis()->GetLast();
-        m_ph_kfactor_correct->at(photon) = _h_kfactor_sl->GetBinContent(kfactor_pt_bin_number);
+        // Prompt derive fit weight
+        TF1 *_ppt_prompt_fit = (TF1*)_ppt_prompt->GetFunction("pol1");
+        m_weight_PPT_prompt_fit = _ppt_prompt_fit->Eval(PPT_x_value);
+        // Prompt derive bin weight
+        int promt_bin_number = _ppt_prompt->GetXaxis()->FindBin(PPT_x_value);
+        m_weight_PPT_prompt_bin = _ppt_prompt->GetBinContent(promt_bin_number);
+        
       }
-      if (is_dilepton){
-        int kfactor_pt_bin_number = _h_kfactor_dl->GetXaxis()->FindBin(photon_pt);
-        if (photon_pt >= _h_kfactor_dl->GetXaxis()->GetXmax())
-          kfactor_pt_bin_number = _h_kfactor_dl->GetXaxis()->GetLast();
-        m_ph_kfactor_correct->at(photon) = _h_kfactor_dl->GetBinContent(kfactor_pt_bin_number);
+      delete _ppt_hfake_fit;
+      delete _ppt_prompt_fit;
+      
+      // Add in the Kfactor weights
+      m_ph_kfactor_correct->at(photon)=1;
+      if(kfactor_applied){
+        float photon_pt = ph_pt->at(photon)*0.001;
+        if(is_singlelepton){
+          int kfactor_pt_bin_number = _h_kfactor_sl->GetXaxis()->FindBin(photon_pt);
+          if (photon_pt >= _h_kfactor_sl->GetXaxis()->GetXmax())
+            kfactor_pt_bin_number = _h_kfactor_sl->GetXaxis()->GetLast();
+          m_ph_kfactor_correct->at(photon) = _h_kfactor_sl->GetBinContent(kfactor_pt_bin_number);
+        }
+        if (is_dilepton){
+          int kfactor_pt_bin_number = _h_kfactor_dl->GetXaxis()->FindBin(photon_pt);
+          if (photon_pt >= _h_kfactor_dl->GetXaxis()->GetXmax())
+            kfactor_pt_bin_number = _h_kfactor_dl->GetXaxis()->GetLast();
+          m_ph_kfactor_correct->at(photon) = _h_kfactor_dl->GetBinContent(kfactor_pt_bin_number);
+        }
       }
-    }
-    m_efake_sf_Nominal->at(photon)=1;
-    m_efake_sf_RangeDown->at(photon)=1;
-    m_efake_sf_BkgFunc->at(photon)=1;
-    m_efake_sf_SigFunc->at(photon)=1;
-    m_efake_sf_TypeC->at(photon)=1;
-    m_efake_sf_Stat_Pt2Eta4->at(photon)=1;
-    m_efake_sf_Stat_Pt3Eta3->at(photon)=1;
-    m_efake_sf_Stat_Pt4Eta1->at(photon)=1;
-    m_efake_sf_Stat_Pt4Eta2->at(photon)=1;
-    m_efake_sf_Stat_Pt4Eta3->at(photon)=1;
-    m_efake_sf_Stat_Pt4Eta5->at(photon)=1;
-    m_efake_sf_Total->at(photon)=1;
+      
+      // Add in the eamma fake SFs
+      m_efake_sf_Nominal->at(photon)=1;
+      m_efake_sf_RangeDown->at(photon)=1;
+      m_efake_sf_BkgFunc->at(photon)=1;
+      m_efake_sf_SigFunc->at(photon)=1;
+      m_efake_sf_TypeC->at(photon)=1;
+      m_efake_sf_Stat_Pt2Eta4->at(photon)=1;
+      m_efake_sf_Stat_Pt3Eta3->at(photon)=1;
+      m_efake_sf_Stat_Pt4Eta1->at(photon)=1;
+      m_efake_sf_Stat_Pt4Eta2->at(photon)=1;
+      m_efake_sf_Stat_Pt4Eta3->at(photon)=1;
+      m_efake_sf_Stat_Pt4Eta5->at(photon)=1;
+      m_efake_sf_Total->at(photon)=1;
+      if(efake_sf_applied){
+        float photon_pt = ph_pt->at(photon)*0.001;
+        float photon_eta = fabs(ph_eta->at(photon));
+        int efake_sf_eta_bin_number = _h_efake_sf_Nominal->GetXaxis()->FindBin(photon_eta);
+        int efake_sf_pt_bin_number = _h_efake_sf_Nominal->GetYaxis()->FindBin(photon_pt);
+        if (efake_sf_eta_bin_number > _h_efake_sf_Nominal->GetNbinsX()) efake_sf_eta_bin_number = _h_efake_sf_Nominal->GetNbinsX();
+        if (efake_sf_pt_bin_number > _h_efake_sf_Nominal->GetNbinsY()) efake_sf_pt_bin_number = _h_efake_sf_Nominal->GetNbinsY();
 
-    if(efake_sf_applied){
-      float photon_pt = ph_pt->at(photon)*0.001;
-      float photon_eta = fabs(ph_eta->at(photon));
-      int efake_sf_eta_bin_number = _h_efake_sf_Nominal->GetXaxis()->FindBin(photon_eta);
-      int efake_sf_pt_bin_number = _h_efake_sf_Nominal->GetYaxis()->FindBin(photon_pt);
-      if (efake_sf_eta_bin_number > _h_efake_sf_Nominal->GetNbinsX()) efake_sf_eta_bin_number = _h_efake_sf_Nominal->GetNbinsX();
-      if (efake_sf_pt_bin_number > _h_efake_sf_Nominal->GetNbinsY()) efake_sf_pt_bin_number = _h_efake_sf_Nominal->GetNbinsY();
+        m_efake_sf_Nominal->at(photon) = _h_efake_sf_Nominal->GetBinContent(efake_sf_eta_bin_number, efake_sf_pt_bin_number);
+        m_efake_sf_Nominal->at(photon) = _h_efake_sf_Nominal->GetBinContent(efake_sf_eta_bin_number, efake_sf_pt_bin_number);
+        m_efake_sf_RangeDown->at(photon) = _h_efake_sf_RangeDown->GetBinContent(efake_sf_eta_bin_number, efake_sf_pt_bin_number);
+        m_efake_sf_BkgFunc->at(photon) = _h_efake_sf_BkgFunc->GetBinContent(efake_sf_eta_bin_number, efake_sf_pt_bin_number);
+        m_efake_sf_SigFunc->at(photon) = _h_efake_sf_SigFunc->GetBinContent(efake_sf_eta_bin_number, efake_sf_pt_bin_number);
+        m_efake_sf_TypeC->at(photon) = _h_efake_sf_TypeC->GetBinContent(efake_sf_eta_bin_number, efake_sf_pt_bin_number);
+        m_efake_sf_Stat_Pt2Eta4->at(photon) = _h_efake_sf_Stat_Pt2Eta4->GetBinContent(efake_sf_eta_bin_number, efake_sf_pt_bin_number);
+        m_efake_sf_Stat_Pt3Eta3->at(photon) = _h_efake_sf_Stat_Pt3Eta3->GetBinContent(efake_sf_eta_bin_number, efake_sf_pt_bin_number);
+        m_efake_sf_Stat_Pt4Eta1->at(photon) = _h_efake_sf_Stat_Pt4Eta1->GetBinContent(efake_sf_eta_bin_number, efake_sf_pt_bin_number);
+        m_efake_sf_Stat_Pt4Eta2->at(photon) = _h_efake_sf_Stat_Pt4Eta2->GetBinContent(efake_sf_eta_bin_number, efake_sf_pt_bin_number);
+        m_efake_sf_Stat_Pt4Eta3->at(photon) = _h_efake_sf_Stat_Pt4Eta3->GetBinContent(efake_sf_eta_bin_number, efake_sf_pt_bin_number);
+        m_efake_sf_Stat_Pt4Eta5->at(photon) = _h_efake_sf_Stat_Pt4Eta5->GetBinContent(efake_sf_eta_bin_number, efake_sf_pt_bin_number);
+        m_efake_sf_Total->at(photon) = _h_efake_sf_Total->GetBinContent(efake_sf_eta_bin_number, efake_sf_pt_bin_number);
+      }
+      
+      // Add in the hadronic fake SFs
+      m_hadFake_sf_Nominal->at(photon) = 1;
+      m_hadFake_sf_ttbarModelling_Sherpa->at(photon) = 1;
+      m_hadFake_sf_ttbarModelling_ISRFSRUp->at(photon) = 1;
+      m_hadFake_sf_ttbarModelling_ISRFSRDn->at(photon) = 1;
+      m_hadFake_sf_BkgSubtraction_ttgamma->at(photon) = 1;
+      m_hadFake_sf_BkgSubtraction_rest->at(photon) = 1;
 
-      m_efake_sf_Nominal->at(photon) = _h_efake_sf_Nominal->GetBinContent(efake_sf_eta_bin_number, efake_sf_pt_bin_number);
-      m_efake_sf_Nominal->at(photon) = _h_efake_sf_Nominal->GetBinContent(efake_sf_eta_bin_number, efake_sf_pt_bin_number);
-      m_efake_sf_RangeDown->at(photon) = _h_efake_sf_RangeDown->GetBinContent(efake_sf_eta_bin_number, efake_sf_pt_bin_number);
-      m_efake_sf_BkgFunc->at(photon) = _h_efake_sf_BkgFunc->GetBinContent(efake_sf_eta_bin_number, efake_sf_pt_bin_number);
-      m_efake_sf_SigFunc->at(photon) = _h_efake_sf_SigFunc->GetBinContent(efake_sf_eta_bin_number, efake_sf_pt_bin_number);
-      m_efake_sf_TypeC->at(photon) = _h_efake_sf_TypeC->GetBinContent(efake_sf_eta_bin_number, efake_sf_pt_bin_number);
-      m_efake_sf_Stat_Pt2Eta4->at(photon) = _h_efake_sf_Stat_Pt2Eta4->GetBinContent(efake_sf_eta_bin_number, efake_sf_pt_bin_number);
-      m_efake_sf_Stat_Pt3Eta3->at(photon) = _h_efake_sf_Stat_Pt3Eta3->GetBinContent(efake_sf_eta_bin_number, efake_sf_pt_bin_number);
-      m_efake_sf_Stat_Pt4Eta1->at(photon) = _h_efake_sf_Stat_Pt4Eta1->GetBinContent(efake_sf_eta_bin_number, efake_sf_pt_bin_number);
-      m_efake_sf_Stat_Pt4Eta2->at(photon) = _h_efake_sf_Stat_Pt4Eta2->GetBinContent(efake_sf_eta_bin_number, efake_sf_pt_bin_number);
-      m_efake_sf_Stat_Pt4Eta3->at(photon) = _h_efake_sf_Stat_Pt4Eta3->GetBinContent(efake_sf_eta_bin_number, efake_sf_pt_bin_number);
-      m_efake_sf_Stat_Pt4Eta5->at(photon) = _h_efake_sf_Stat_Pt4Eta5->GetBinContent(efake_sf_eta_bin_number, efake_sf_pt_bin_number);
-      m_efake_sf_Total->at(photon) = _h_efake_sf_Total->GetBinContent(efake_sf_eta_bin_number, efake_sf_pt_bin_number);
-    }
+      if(hadFake_sf_applied)
+      {
+        //Apply had-fake SF only for tight and isolated photons:
+        if( ph_isTight->at(photon) && ph_isoFCT->at(photon))
+        {
+          float ph_pt = ph_pt->at(photon)*0.001;
+          float ph_absEta = fabs(ph_eta->at(photon));
+          int bin_number = 0;
+
+          if(ph_conversionType->at(photon)==1)
+          { //Unconverted photons:
+            if(absEta<0.60){
+              if(pt>=20 && pt<30) bin_number = 1;
+              if(pt>=30) bin_number = 2;
+            }
+            if(absEta>=0.60 && absEta<1.37){
+              if(pt>=20 && pt<30) bin_number = 3;
+              if(pt>=30) bin_number = 4;
+            }
+            if(absEta>1.52 && absEta<=2.37){
+              bin_number = 5;
+            }
+          }
+          else
+          { //Converted photons:
+            if(absEta<0.60)
+              bin_number = 1;
+            if(absEta>=0.60 && absEta<1.37){
+              if(pt>=20 && pt<30) bin_number = 2;
+              if(pt>=30) bin_number = 3;
+            }
+            if(absEta>1.52 && absEta<=2.37){
+              if(pt>=20 && pt<50) bin_number = 4;
+              if(pt>=50) bin_number = 5;
+            }
+          }
+          
+          if(bin_number!=0){
+            m_hadFake_sf_Nominal->at(photon) = _hfakeSF_conv_Nominal->GetBinContent(bin_number);
+            m_hadFake_sf_ttbarModelling_Sherpa->at(photon) = _hfakeSF_conv_ttbarModelling_Sherpa->GetBinContent(bin_number);
+            m_hadFake_sf_ttbarModelling_ISRFSRUp->at(photon) = _hfakeSF_conv_ttbarModelling_ISRFSRUp->GetBinContent(bin_number);
+            m_hadFake_sf_ttbarModelling_ISRFSRDn->at(photon) = _hfakeSF_conv_ttbarModelling_ISRFSRDown->GetBinContent(bin_number);
+            m_hadFake_sf_BkgSubtraction_ttgamma->at(photon) = _hfakeSF_conv_BkgSubtraction_ttgamma->GetBinContent(bin_number);
+            m_hadFake_sf_BkgSubtraction_rest->at(photon) = _hfakeSF_conv_BkgSubtraction_rest->GetBinContent(bin_number);
+          }
+        }
+      }
 
     } // end loop over photons
 
@@ -522,6 +646,9 @@ int main(int argc, char** argv)
       // add efake sf files
         m_add_efake_sf(newfile,
           "EFakeSFs_Final.root");
+      
+      // add hadronic fake sf files
+      m_add_hadFake_sf( newfile, "hfake_SF_final_3D.root" )
 
       oldFile = new TFile((file.c_str()), "read");
 
